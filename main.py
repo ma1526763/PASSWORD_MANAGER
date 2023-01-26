9ccc1e51c7189125369b293c1fb60834b6356666
aliriax <ma1526763@gmail.com>
2023-01-26 19:37:50 +0500

    JSON

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-
 # def automatic_password_to_clipboard(password):
 #     clip = Tk()
 #     clip.withdraw()
